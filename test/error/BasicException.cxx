704ac4688930e4253a34c437f169bcc2a5ab64db
Andrew Brown <adb1413@rit.edu>
2010-10-30 19:27:26 -0400

    Improved BasicExceptionTest

--- test/error/BasicException.cxx
+++ test/error/BasicException.cxx
@@ -6,9 +6,12 @@
  */
 #include "BasicException.hpp"
 
+class BasicExceptionTest {
+public:
+	void testThrowCatch();
+};
 
-int main(int argc,
-         char *argv[]) {
+void BasicExceptionTest::testThrowCatch() {
 	
 	int line=45;
 	
@@ -19,6 +22,17 @@ int main(int argc,
 		throw e;
 	} catch (BasicException &e) {
 		cout << e.what() << endl;
+		return;
 	}
+	cerr << "Exception was not caught!" << endl;
+	cerr << "TEST FAILED!" << endl;
+}
+
+int main(int argc, char *argv[]) {
+	
+	BasicExceptionTest test;
+	
+	test.testThrowCatch();
+	cout << "PASSED ALL TESTS." << endl;
 }
 
