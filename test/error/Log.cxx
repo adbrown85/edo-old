1267d28bfb7ae6ac06a1435fa5c326c980465808
Andrew Brown <adb1413@rit.edu>
2010-10-30 19:46:48 -0400

    Improved Log test

--- test/error/Log.cxx
+++ test/error/Log.cxx
@@ -5,45 +5,82 @@
  *     Andrew Brown <adb1413@rit.edu>
  */
 #include "Log.hpp"
+#include <cassert>
 
 
-/* Fake to test log listener */
+/* Fake listener for log. */
 class FakeLogListener : public LogListener {
 public:
+	FakeLogListener();
 	virtual void onLogUpdate(const string &text);
+	virtual int getTally();
+private:
+	int tally;
 };
-void FakeLogListener::onLogUpdate(const string &text) {
-	cout << "Log updated!" << endl;
+
+/* Initializes the listener. */
+FakeLogListener::FakeLogListener() {
+	tally = 0;
 }
 
+/* Prints a message and increases the tally. */
+void FakeLogListener::onLogUpdate(const string &text) {
+	cerr << "Log updated!" << endl;
+	++tally;
+}
 
-int main(int argc, char *argv[]) {
+/* Return number of times a message was received. */
+int FakeLogListener::getTally() {
+	return tally;
+}
+
+/** Test fixture for Log. */
+class LogTest {
+public:
+	void testInsertion();
+	void testIterator();
+};
+
+/** Ensures messages can be inserted and that listeners get notifications.
+ * 
+ * Should print three messages to standard error from testInsertion.  For each 
+ * message there should also be a line between saying "Log updated!", which is
+ * printed by FakeLogListener.
+ */
+void LogTest::testInsertion() {
 	
-	Log::iterator it;
+	FakeLogListener listener;
 	
-	// Start
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "Log" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
-	
-	// Test
-	glog.addListener(new FakeLogListener());
+	glog.addListener(&listener);
 	glog << "This is a message!" << endl;
 	glog << "This is a message with a " << 1.5 << endl;
 	glog << "This is a message with a " << 1 << endl;
-	
-	for (it=glog.begin(); it!=glog.end(); ++it) {
-		cout << "  " << *it << endl;
-	}
-	
-	// Finish
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "Log" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
-	return 0;
+	assert(listener.getTally() == 3);
+	cout << "PASSED" << endl;
 }
 
+/** Makes sure the log stores its messages and that they're accessible. */
+void LogTest::testIterator() {
+	
+	Log::iterator it;
+	int messages;
+	
+	messages = 0;
+	for (it=glog.begin(); it!=glog.end(); ++it) {
+		cout << "  " << *it << endl;
+		++messages;
+	}
+	assert(messages == 3);
+	cout << "PASSED" << endl;
+}
+
+/* Runs the test. */
+int main(int argc, char *argv[]) {
+	
+	LogTest test;
+	
+	test.testInsertion();
+	test.testIterator();
+	cout << "ALL TESTS PASSED" << endl;
+	return 0;
+}
