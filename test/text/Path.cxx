6288e49b883c787dd255c1c0a9ffcb2dfb2213e4
Andrew Brown <adb1413@rit.edu>
2010-10-30 23:41:52 -0400

    Fixed up Path test

--- test/text/Path.cxx
+++ test/text/Path.cxx
@@ -7,7 +7,6 @@
 #include "Path.hpp"
 #include <cassert>
 
-
 /* Unit test for Path. */
 class PathTest {
 public:
@@ -18,198 +17,151 @@ public:
 	void testInternals();
 	void testRelativePath();
 	void testTokenize();
-protected:
-	void print(string result);
-	void testExpandWith(string);
-	void testExtensionWith(string path, string answer);
-	void testRelativePathWith(string A, string B, string answer);
 };
-
-
-void PathTest::print(string result) {
-	
-	if (result.empty()) {
-		cout << "  {empty}" << endl;
-	} else {
-		cout << "  " << result << endl;
-	}
-}
-
 
 void PathTest::testBasename() {
 	
-	cout << "\nTesting getBasename" << endl;
-	print(Path::getBasename("input/scene.xml"));
+	string result;
+	
+	cout << "PathTest::testBasename" << endl;
+	result = Path::getBasename("input/scene.xml");
+	assert(result == "scene.xml");
+	cout << "PASSED" << endl;
 }
-
 
 void PathTest::testDirname() {
 	
-	cout << "\nTesting getDirname" << endl;
-	print(Path::getDirname("../../scene.xml"));
-	print(Path::getDirname("input/scene.xml"));
-	print(Path::getDirname("scene.xml"));
-	print(Path::getDirname("/scene.xml"));
+	string result;
+	
+	cout << "PathTest::testDirname" << endl;
+	result = Path::getDirname("../../scene.xml");
+	assert(result == "../..");
+	result = Path::getDirname("input/scene.xml");
+	assert(result == "input");
+	result = Path::getDirname("scene.xml");
+	assert(result == ".");
+	result = Path::getDirname("/scene.xml");
+	assert(result == "/");
+	cout << "PASSED" << endl;
 }
-
 
 void PathTest::testExtension() {
 	
-	cout << "\nTesting extension" << endl;
-	testExtensionWith("scene.xml", "xml");
-}
-
-
-void PathTest::testExtensionWith(string path, string answer) {
-	
 	string result;
 	
-	print(path);
-	result = Path::getExtension(path);
-	print(result);
-	assert(result == answer);
+	cout << "PathTest::testExtension" << endl;
+	result = Path::getExtension("scene.xml");
+	assert(result == "xml");
+	cout << "PASSED" << endl;
 }
-
 
 void PathTest::testExpand() {
 	
-	cout << "\nTesting expand" << endl;
-	testExpandWith("${GANDER}/glsl/outline.glsl");
-	testExpandWith("/usr/local/gander/glsl/outline.glsl");
-}
-
-
-void PathTest::testExpandWith(string path) {
-	
 	string result;
 	
-	result = Path::expand(path);
-	print(result);
+	cout << "PathTest::testExpand" << endl;
+	result = Path::expand("${USER}/glsl/outline.glsl");
+	assert(result == string(getenv("USER")) + "/glsl/outline.glsl");
+	result = Path::expand("/usr/local/gander/glsl/outline.glsl");
+	assert(result == "/usr/local/gander/glsl/outline.glsl");
+	cout << "PASSED" << endl;
 }
-
 
 void PathTest::testInternals() {
 	
-	cout << "\nTesting getInternals" << endl;
-	print(Path::getInternals("input/scene.xml"));
-	print(Path::getInternals("/input/scene.xml"));
-	print(Path::getInternals("./input/scene.xml"));
-	print(Path::getInternals("../../input/scene.xml"));
-	print(Path::getInternals("scene.xml"));
-	print(Path::getInternals("gander/input/scene.xml"));
-	print(Path::getInternals("C:\\scene.xml"));
-	print(Path::getInternals("C:\\gander/input/scene.xml"));
+	string result;
+	
+	cout << "PathTest::testInternals" << endl;
+	result = Path::getInternals("input/scene.xml");
+	assert(result == "input");
+	result = Path::getInternals("/input/scene.xml");
+	assert(result == "input");
+	result = Path::getInternals("./input/scene.xml");
+	assert(result == "input");
+	result = Path::getInternals("../../input/scene.xml");
+	assert(result == "../../input");
+	result = Path::getInternals("scene.xml");
+	assert(result == "");
+	result = Path::getInternals("gander/input/scene.xml");
+	assert(result == "gander/input");
+	result = Path::getInternals("C:\\scene.xml");
+	assert(result == "");
+	result = Path::getInternals("C:\\gander/input/scene.xml");
+	assert(result == "gander/input");
+	cout << "PASSED" << endl;
 }
-
 
 void PathTest::testRelativePath() {
 	
-	// Test both relative
-	cout << "\nWhen both A and B are relative..." << endl;
-	testRelativePathWith("input\\scene.xml",
-	                     "glsl/file.frag",
-	                     "input/glsl/file.frag");
-	testRelativePathWith("input/scene.xml",
-	                     "../glsl/file.frag",
-	                     "glsl/file.frag");
-	testRelativePathWith("input/scene.xml",
-	                     "../../../glsl/file.frag",
-	                     "../../glsl/file.frag");
-	testRelativePathWith("../../input/basic.vert",
-	                     "file.frag",
-	                     "../../input/file.frag");
-	
-	// Test if any are absolute paths
-	cout << "\nWhen A or B is absolute..." << endl;
-	testRelativePathWith("input/scene.xml",
-	                     "/home/user/gander/glsl/file.frag",
-	                     "/home/user/gander/glsl/file.frag");
-	testRelativePathWith("/home/user/gander/input/scene.xml",
-	                     "glsl/file.frag",
-	                     "/home/user/gander/input/glsl/file.frag");
-	testRelativePathWith("/home/user/gander/input/scene.xml",
-	                     "../glsl/file.frag",
-	                     "/home/user/gander/glsl/file.frag");
-	
-	// Test if A has no directory
-	cout << "\nWhen A has no directory..." << endl;
-	testRelativePathWith("scene.xml",
-	                     "../../glsl/file.frag",
-	                     "../../glsl/file.frag");
-	
-	// Test if A is in root
-	cout << "\nWhen A is in root..." << endl;
-	try {
-		testRelativePathWith("/scene.xml",
-		                     "../glsl/file.frag",
-		                     "/glsl/file.frag");
-		assert(false);
-	} catch (BasicException &e) {
-		cerr << e.what() << endl;
-	}
-	try {
-		testRelativePathWith("C:\\scene.xml",
-		                     "../glsl/file.frag",
-		                     "C:\\glsl/file.frag");
-		assert(false);
-	} catch (BasicException &e) {
-		cerr << e.what() << endl;
-	}
-}
-
-
-void PathTest::testRelativePathWith(string A, string B, string answer) {
-	
 	string result;
 	
-	// Get result and print
-	cout << "Testing relative path..." << endl;
-	cout << "  A: " << A << endl;
-	cout << "  B: " << B << endl;
-	result = Path::getRelativePath(A, B);
-	print(result);
-	assert(result == answer);
+	cout << "PathTest::testRelativePath" << endl;
+	
+	// Test both relative
+	result = Path::getRelativePath("input\\scene.xml", "glsl/file.frag");
+	assert(result == "input/glsl/file.frag");
+	result = Path::getRelativePath("input/scene.xml", "../glsl/file.frag");
+	assert(result == "glsl/file.frag");
+	result = Path::getRelativePath("input/scene.xml", "../../glsl/file.frag");
+	assert(result == "../glsl/file.frag");
+	result = Path::getRelativePath("../../input/basic.vert", "file.frag");
+	assert(result == "../../input/file.frag");
+	
+	// Test if any are absolute paths
+	result = Path::getRelativePath(
+			"input/scene.xml",
+			"/home/user/gander/glsl/file.frag");
+	assert(result == "/home/user/gander/glsl/file.frag");
+	result = Path::getRelativePath(
+			"/home/user/gander/input/scene.xml",
+			"glsl/file.frag");
+	assert(result == "/home/user/gander/input/glsl/file.frag");
+	result = Path::getRelativePath(
+			"/home/user/gander/input/scene.xml",
+			"../glsl/file.frag");
+	assert(result == "/home/user/gander/glsl/file.frag");
+	
+	// Test if A has no directory
+	result = Path::getRelativePath("scene.xml", "../../glsl/file.frag");
+	assert(result == "../../glsl/file.frag");
+	
+	// Test if A is in root
+	try {
+		Path::getRelativePath("/scene.xml", "../glsl/file.frag");
+		assert(false);
+	} catch (BasicException &e) {}
+	try {
+		Path::getRelativePath("C:\\scene.xml", "../glsl/file.frag");
+		assert(false);
+	} catch (BasicException &e) {}
+	
+	cout << "PASSED" << endl;
 }
-
 
 void PathTest::testTokenize() {
 	
 	vector<string> tokens;
 	
-	cout << "\nTesting tokenize" << endl;
+	cout << "PathTest::testTokenize" << endl;
 	Path::tokenize("input/scene.xml", tokens);
-	for (size_t i=0; i<tokens.size(); ++i) {
-		print(tokens[i]);
-	}
+	assert(tokens[0] == "input");
+	assert(tokens[1] == "scene.xml");
+	cout << "PASSED" << endl;
 }
-
 
 /* Run test. */
 int main() {
 	
 	PathTest test;
 	
-	// Start
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "Path" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
-	
-	// Test
 	test.testBasename();
 	test.testDirname();
 	test.testExtension();
+	test.testExpand();
 	test.testInternals();
 	test.testTokenize();
 	test.testRelativePath();
-	
-	// Finish
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "Path" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
+	cout << "ALL TESTS PASSED" << endl;
 	return 0;
 }
 
